c1b0bb8d1fa83c5a70d655eeca37d32be286b20a
Greg Holmes <greg.holmes2@vonage.com>
2020-10-06 21:53:19 +0100

    Added first commit

---
+++ react-to-do/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+
+function Todo({ todo }) {
+  return (
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Chris the troll" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  const addToDo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        <TodoForm addTodo={addToDo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
